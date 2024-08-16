6dc43b3a94a74aa6cc66fde9b07b13326b233342
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2024-08-16 23:29:06 +0300

    Updated

--- setup.py
+++ setup.py
@@ -42,62 +42,73 @@ def get_version(rel_path):
     raise RuntimeError('Unable to find version string.')
 
 
-# HERE = os.path.abspath(os.path.dirname(__file__))
+## Basic information
 HERE = pathlib.Path(__file__).parent
 README = (HERE / 'README.md').read_text(encoding='utf-8')
 VERSION = get_version('scikitplot/__init__.py')
 
+## Define the keywords
+KEYWORDS = (
+    "visualization",
+    "machine learning",
+    "scikit-learn",
+    "matplotlib",
+    "data science",
+)
+
+##########################################################################
+## Define the configuration, Run setup script
+##########################################################################
+
 setup(
-    name='scikit-plot',
-    description='An intuitive library to add plotting functionality to scikit-learn objects.',
-    long_description_content_type="text/markdown",
-    long_description=README,
+    name='scikitplot',
     version=VERSION,
-    url='https://github.com/celik-muhammed/scikit-plot',
-    download_url='https://github.com/celik-muhammed/scikit-plot/tree/muhammed-dev',
+    description='An intuitive library to add plotting functionality to scikit-learn objects.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords=KEYWORDS,
+    license='MIT License',
+    author='Reiichiro Nakano et al.',  # Your name first as you're the current maintainer
+    author_email='reiichiro.s.nakano@gmail.com',  # Your email address
+    url='https://github.com/celik-muhammed/scikit-plot/tree/muhammed-dev',  # Your fork's URL
+    maintainer='Muhammed Çelik',
+    maintainer_email='muhammed.business.network@gmail.com',
     project_urls={
         'Documentation': 'https://scikit-plot.readthedocs.io/en/stable/',
-        'Source Code': 'https://github.com/reiinakano/scikit-plot',
-        'Bug Tracker': 'https://github.com/reiinakano/scikit-plot/issues',
-        'Forum': 'https://github.com/reiinakano/scikit-plot',
-        'Donate': 'https://github.com/reiinakano/scikit-plot',
+        'Source Code': 'https://github.com/celik-muhammed/scikit-plot',  # Updated to your fork's URL
+        'Bug Tracker': 'https://github.com/celik-muhammed/scikit-plot/issues',  # Updated to your fork's issues URL
+        'Forum': 'https://github.com/celik-muhammed/scikit-plot/issues',  # Updated forum link
+        'Donate': 'https://github.com/celik-muhammed/scikit-plot#donate',  # Updated donation link
     },
-    author='Muhammed Çelik, Reiichiro Nakano',
-    author_email='reiichiro.s.nakano@gmail.com',
-    license='MIT License',
+    download_url='https://github.com/celik-muhammed/scikit-plot/tree/muhammed-dev',  # Your fork's download URL
+    # packages=find_packages(),  # Finds all packages automatically
+    packages=['scikitplot'],
+    include_package_data=True,
+    platforms='any',
+    # entry_points={"console_scripts": []},
     install_requires=[
         'matplotlib>=1.4.0',
         'scikit-learn>=0.21',
         'scipy>=0.9',
         'joblib>=0.10'
     ],
-    # Supported Python versions
-    # python_requires=">=2.7",
-    classifiers = [
-        'Development Status :: 1 - Planning',
+    classifiers=[
+        'Development Status :: 4 - Beta',  # Change status as per the current state
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.14',
-        'Topic :: Communications :: Email',
-        'Topic :: Documentation :: Sphinx',
         'Topic :: Scientific/Engineering :: Visualization',
-        'Topic :: Utilities',
-        ],
-    packages=['scikitplot'],
-    include_package_data=True,
-    platforms='any',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    cmdclass={'test': PyTest},
     tests_require=['pytest'],
     test_suite='scikitplot.tests.test_scikitplot',
-    cmdclass={'test': PyTest},
     extras_require={
         'testing': ['pytest'],
     }
