5440ef9732cda189df9a366aa0576a09b42c1ab7
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2024-08-16 09:40:21 +0000

    Change version '0.3.7' to '0.3.7.1'

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
+"""
+Setup script.
+"""
+import os
+import io
+import sys
+import codecs
+import pathlib
+
 from __future__ import print_function
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-import io
-import codecs
-import os
-import sys
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
-long_description = read('README.md')
-# long_description = ''
 
 
 class PyTest(TestCommand):
@@ -33,27 +23,47 @@ class PyTest(TestCommand):
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
+def read(*filenames, **kwargs):
+    here = os.path.abspath(os.path.dirname(__file__))
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(os.path.join(here, filename)), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError('Unable to find version string.')
+
+
+# HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = pathlib.Path(__file__).parent
+README = (HERE / 'README.md').read_text()
+VERSION = get_version('scikitplot/__init__.py')
+
 setup(
     name='scikit-plot',
-    version='0.3.7',
+    description='An intuitive library to add plotting functionality to scikit-learn objects.',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    version=VERSION,
     url='https://github.com/reiinakano/scikit-plot',
-    license='MIT License',
     author='Reiichiro Nakano',
-    tests_require=['pytest'],
+    author_email='reiichiro.s.nakano@gmail.com',
+    license='MIT License',
     install_requires=[
         'matplotlib>=1.4.0',
         'scikit-learn>=0.18',
         'scipy>=0.9',
         'joblib>=0.10'
     ],
-    cmdclass={'test': PyTest},
-    author_email='reiichiro.s.nakano@gmail.com',
-    description='An intuitive library to add plotting functionality to scikit-learn objects.',
-    long_description=long_description,
-    packages=['scikitplot'],
-    include_package_data=True,
-    platforms='any',
-    test_suite='scikitplot.tests.test_scikitplot',
+    # Supported Python versions
+    # python_requires=">=2.7",
     classifiers = [
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
@@ -61,13 +71,19 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Natural Language :: English',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         ],
+    packages=['scikitplot'],
+    include_package_data=True,
+    platforms='any',
+    tests_require=['pytest'],
+    test_suite='scikitplot.tests.test_scikitplot',
+    cmdclass={'test': PyTest},
     extras_require={
         'testing': ['pytest'],
     }
