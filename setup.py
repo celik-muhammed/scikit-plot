39078d7428825612bf9d72bdc54678ff874ab3d2
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2024-08-19 17:20:20 +0000

    Updated

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class PyTest(TestCommand):
 ## Get the directory where setup.py is located
 HERE         = pathlib.Path(__file__).parent
 NAME         = 'scikit-plots'
-PACKAGE      = find_packages(where='scikitplot')
+PACKAGE      = find_packages(where='.', include=['scikitplot', 'scikitplot.utils'])
 VERSION_PATH = os.path.join(HERE, 'scikitplot/__init__.py')
 DESCRIPTION  = 'An intuitive library to add plotting functionality to scikit-learn objects.'
 ## Read the contents of the README file
